c22d3dd8dbbc2efcd4e67a417d882aaa66e6fb45
sunp1234 <44682042+sunp1234@users.noreply.github.com>
2025-03-07 02:15:36 -0500

    Add files via upload

---
+++ PSun-Ass5.sql
@@ -0,0 +1,295 @@
+-- MySQL dump 10.13  Distrib 8.0.41, for Win64 (x86_64)
+--
+-- Host: localhost    Database: unitedhelpers
+-- ------------------------------------------------------
+-- Server version	8.0.41
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!50503 SET NAMES utf8 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Table structure for table `assignment`
+--
+
+DROP TABLE IF EXISTS `assignment`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `assignment` (
+  `volunteerId` int NOT NULL,
+  `taskCode` int NOT NULL,
+  `startDateTime` datetime NOT NULL,
+  `endDateTime` datetime NOT NULL,
+  PRIMARY KEY (`volunteerId`,`taskCode`),
+  KEY `taskCode` (`taskCode`),
+  CONSTRAINT `assignment_ibfk_1` FOREIGN KEY (`volunteerId`) REFERENCES `volunteer` (`volunteerId`),
+  CONSTRAINT `assignment_ibfk_2` FOREIGN KEY (`taskCode`) REFERENCES `task` (`taskCode`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `assignment`
+--
+
+LOCK TABLES `assignment` WRITE;
+/*!40000 ALTER TABLE `assignment` DISABLE KEYS */;
+INSERT INTO `assignment` VALUES (1,1,'2025-03-03 07:30:00','2025-03-03 11:30:00'),(2,2,'2025-03-04 10:00:00','2025-03-04 15:00:00');
+/*!40000 ALTER TABLE `assignment` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `item`
+--
+
+DROP TABLE IF EXISTS `item`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `item` (
+  `itemId` int NOT NULL AUTO_INCREMENT,
+  `itemDescription` varchar(255) NOT NULL,
+  `itemValue` decimal(6,2) DEFAULT NULL,
+  `quantityOnHand` int NOT NULL,
+  PRIMARY KEY (`itemId`)
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `item`
+--
+
+LOCK TABLES `item` WRITE;
+/*!40000 ALTER TABLE `item` DISABLE KEYS */;
+INSERT INTO `item` VALUES (1,'Bandage Set',18.50,120),(2,'Box of Granola Bars',6.75,450);
+/*!40000 ALTER TABLE `item` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `package`
+--
+
+DROP TABLE IF EXISTS `package`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `package` (
+  `packageId` int NOT NULL AUTO_INCREMENT,
+  `taskCode` int NOT NULL,
+  `packageTypeId` int NOT NULL,
+  `packageCreateDate` date NOT NULL,
+  `packageWeight` decimal(5,2) DEFAULT NULL,
+  PRIMARY KEY (`packageId`),
+  KEY `taskCode` (`taskCode`),
+  KEY `packageTypeId` (`packageTypeId`),
+  CONSTRAINT `package_ibfk_1` FOREIGN KEY (`taskCode`) REFERENCES `task` (`taskCode`),
+  CONSTRAINT `package_ibfk_2` FOREIGN KEY (`packageTypeId`) REFERENCES `package_type` (`packageTypeId`)
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `package`
+--
+
+LOCK TABLES `package` WRITE;
+/*!40000 ALTER TABLE `package` DISABLE KEYS */;
+INSERT INTO `package` VALUES (1,2,1,'2025-03-03',4.80),(2,2,2,'2025-03-04',3.60);
+/*!40000 ALTER TABLE `package` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `package_contents`
+--
+
+DROP TABLE IF EXISTS `package_contents`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `package_contents` (
+  `itemId` int NOT NULL,
+  `packageId` int NOT NULL,
+  `itemQuantity` int NOT NULL,
+  PRIMARY KEY (`itemId`,`packageId`),
+  KEY `packageId` (`packageId`),
+  CONSTRAINT `package_contents_ibfk_1` FOREIGN KEY (`itemId`) REFERENCES `item` (`itemId`),
+  CONSTRAINT `package_contents_ibfk_2` FOREIGN KEY (`packageId`) REFERENCES `package` (`packageId`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `package_contents`
+--
+
+LOCK TABLES `package_contents` WRITE;
+/*!40000 ALTER TABLE `package_contents` DISABLE KEYS */;
+INSERT INTO `package_contents` VALUES (1,1,12),(2,2,8);
+/*!40000 ALTER TABLE `package_contents` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `package_type`
+--
+
+DROP TABLE IF EXISTS `package_type`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `package_type` (
+  `packageTypeId` int NOT NULL AUTO_INCREMENT,
+  `packageTypeName` varchar(50) NOT NULL,
+  PRIMARY KEY (`packageTypeId`)
+) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `package_type`
+--
+
+LOCK TABLES `package_type` WRITE;
+/*!40000 ALTER TABLE `package_type` DISABLE KEYS */;
+INSERT INTO `package_type` VALUES (1,'Medical Supplies'),(2,'Infant Care'),(3,'Non-Perishable Food');
+/*!40000 ALTER TABLE `package_type` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `packing_list`
+--
+
+DROP TABLE IF EXISTS `packing_list`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `packing_list` (
+  `packingListId` int NOT NULL AUTO_INCREMENT,
+  `packingListName` varchar(100) NOT NULL,
+  `packingListDescription` text,
+  PRIMARY KEY (`packingListId`)
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `packing_list`
+--
+
+LOCK TABLES `packing_list` WRITE;
+/*!40000 ALTER TABLE `packing_list` DISABLE KEYS */;
+INSERT INTO `packing_list` VALUES (1,'Emergency Aid Kit','Includes supplies for first response assistance');
+/*!40000 ALTER TABLE `packing_list` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `task`
+--
+
+DROP TABLE IF EXISTS `task`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `task` (
+  `taskCode` int NOT NULL AUTO_INCREMENT,
+  `packingListId` int DEFAULT NULL,
+  `taskTypeId` int NOT NULL,
+  `taskStatusId` int NOT NULL,
+  `taskDescription` text,
+  PRIMARY KEY (`taskCode`),
+  KEY `taskTypeId` (`taskTypeId`),
+  KEY `taskStatusId` (`taskStatusId`),
+  CONSTRAINT `task_ibfk_1` FOREIGN KEY (`taskTypeId`) REFERENCES `task_type` (`taskTypeId`),
+  CONSTRAINT `task_ibfk_2` FOREIGN KEY (`taskStatusId`) REFERENCES `task_status` (`taskStatusId`)
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `task`
+--
+
+LOCK TABLES `task` WRITE;
+/*!40000 ALTER TABLE `task` DISABLE KEYS */;
+INSERT INTO `task` VALUES (1,NULL,1,1,'Assist with phone inquiries and logging requests'),(2,1,2,2,'Organize and distribute 5,000 emergency aid packages');
+/*!40000 ALTER TABLE `task` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `task_status`
+--
+
+DROP TABLE IF EXISTS `task_status`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `task_status` (
+  `taskStatusId` int NOT NULL AUTO_INCREMENT,
+  `taskStatusName` varchar(50) NOT NULL,
+  PRIMARY KEY (`taskStatusId`)
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `task_status`
+--
+
+LOCK TABLES `task_status` WRITE;
+/*!40000 ALTER TABLE `task_status` DISABLE KEYS */;
+INSERT INTO `task_status` VALUES (1,'Ongoing'),(2,'Open');
+/*!40000 ALTER TABLE `task_status` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `task_type`
+--
+
+DROP TABLE IF EXISTS `task_type`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `task_type` (
+  `taskTypeId` int NOT NULL AUTO_INCREMENT,
+  `taskTypeName` varchar(50) NOT NULL,
+  PRIMARY KEY (`taskTypeId`)
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `task_type`
+--
+
+LOCK TABLES `task_type` WRITE;
+/*!40000 ALTER TABLE `task_type` DISABLE KEYS */;
+INSERT INTO `task_type` VALUES (1,'Recurring'),(2,'Packing');
+/*!40000 ALTER TABLE `task_type` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `volunteer`
+--
+
+DROP TABLE IF EXISTS `volunteer`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `volunteer` (
+  `volunteerId` int NOT NULL AUTO_INCREMENT,
+  `volunteerName` varchar(100) NOT NULL,
+  `volunteerAddress` varchar(255) DEFAULT NULL,
+  `volunteerTelephone` varchar(15) DEFAULT NULL,
+  PRIMARY KEY (`volunteerId`)
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `volunteer`
+--
+
+LOCK TABLES `volunteer` WRITE;
+/*!40000 ALTER TABLE `volunteer` DISABLE KEYS */;
+INSERT INTO `volunteer` VALUES (1,'Bucky Barnes','789 Oak St, Greenfield','555-2211'),(2,'Peter Parker','321 Birch Rd, Lakeside','555-3344');
+/*!40000 ALTER TABLE `volunteer` ENABLE KEYS */;
+UNLOCK TABLES;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2025-03-07  2:12:39
